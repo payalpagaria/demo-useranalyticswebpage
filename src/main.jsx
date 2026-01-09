9cde616819926d435170a174e21f288054c8d256
payalpagaria <pagariyapayal67@gmail.com>
2026-01-09 16:32:47 +0530

    basename url changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/demo-useranalyticswebpage">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
